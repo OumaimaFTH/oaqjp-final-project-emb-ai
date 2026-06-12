6bf76526b7921649ca1095e9175da58d2c8b945c
OumaimaFTH <fathi.oumaima11@gmail.com>
2026-06-12 11:23:23 -0400

    task7

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ def sent_emotion_detector():
     """funtion that gets emotion analysis"""
     text_to_analyze = request.args.get("textToAnalyze")
     response =  emotion_detector(text_to_analyze)
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
     formatted_response = (
     f"For the given statement, the system response is "
     f"'anger': {response['anger']}, "
